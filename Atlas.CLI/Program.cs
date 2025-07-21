0f25730933799eaaba17396290e7a3c83ab83dd3
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-07-21 22:26:49 +1000

    Add AtlasCLI project.

---
+++ Atlas.CLI/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using Atlas;
+
+namespace Atlas.CLI;
+
+class CLIOptions
+{
+    [Option('n', "namespace", Required = false, HelpText = "Default C# Namespace for generated code.")]
+    public string Namespace { get; set; }
+
+    [Option('l', "libraryname", Required = true, HelpText = "Name of the C++ library that will hold the exported functions.")]
+    public string LibraryName { get; set; }
+
+    [Option('t', "targetheader", Required = true, HelpText = "Target header file to generate glue for.")]
+    public string TargetHeader { get; set; }
+}
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        return Parser.Default.ParseArguments<CLIOptions>(args)
+            .MapResult(
+                (CLIOptions opts) => RunOptionsAndReturnExitCode(opts),
+                errs => 1);
+    }
+
+    static int RunOptionsAndReturnExitCode(CLIOptions opts)
+    {
+        // Setup Atlas Options
+        Options.Namespace = opts.Namespace;
+        Options.LibraryName = opts.LibraryName;
+
+        // Generate the glue
+        FileInfo targetHeader = new FileInfo(opts.TargetHeader);
+        Output glue = Atlas.GenerateGlue(targetHeader);
+
+        // Write glue to files
+        File.WriteAllText(Path.GetFileNameWithoutExtension(targetHeader.FullName) + ".g.h", glue.CPP);
+        File.WriteAllText(Path.GetFileNameWithoutExtension(targetHeader.FullName) + ".g.cs", glue.CS);
+
+        return 0;
+    }
+}
--- Atlas/Options.cs
+++ Atlas/Options.cs
@@ -4,20 +4,20 @@ namespace Atlas;
 /// <summary>
 /// Definitions for C++ Atlas Syntax.
 /// </summary>
-internal static class Options
+public static class Options
 {
     /// <summary>
     /// Comment that defines a C# export.
     /// </summary>
-    public const string ExportComment = "[CSharpAPI]";
+    public static string ExportComment = "[CSharpAPI]";
 
     /// <summary>
     /// Default C# Namespace for generated code.
     /// </summary>
-    public const string Namespace = "Atlas";
+    public static string Namespace = "Atlas";
 
     /// <summary>
     /// Name of the C++ library that holds the exported functions.
     /// </summary>
-    public const string LibraryName = "Atlas";
+    public static string LibraryName = "Atlas";
 }
