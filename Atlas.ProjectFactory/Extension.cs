1f884f3e1647f55afcbd714fcfacc050aa65da6b
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-07-22 03:02:11 +1000

    Start work on extensions.

---
+++ Atlas.ProjectFactory/Extension.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Extensions;
+
+namespace Atlas.ProjectFactory;
+
+public class Extension : IExtension
+{
+    public void OnLoad()
+    {
+        Console.WriteLine("ProjectFactory extension loaded.");
+    }
+}
---
+++ Atlas/Extensions/IExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas.Extensions;
+
+/// <summary>
+/// Atlas extension interface.
+/// </summary>
+public interface IExtension
+{
+    public void OnLoad();
+}
